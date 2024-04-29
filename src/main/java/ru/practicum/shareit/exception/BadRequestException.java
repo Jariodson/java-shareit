edc09f1f86f245df42cbc877cb19560bf9b27f03
jarodson <jarodson@yandex.ru>
2024-04-29 21:52:09 +0300

    checkstyle

--- src/main/java/ru/practicum/shareit/exception/BadRequestException.java
+++ src/main/java/ru/practicum/shareit/exception/BadRequestException.java
@@ -1,7 +1,7 @@
 package ru.practicum.shareit.exception;
 
-public class BadRequestException extends RuntimeException{
-    public BadRequestException(String m){
+public class BadRequestException extends RuntimeException {
+    public BadRequestException(String m) {
         super(m);
     }
 }
